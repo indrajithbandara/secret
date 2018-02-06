50850cf704f685cddb7e9591dc858d1b64ea166c
david-dacruz <dvddcrz@gmail.com>
2018-02-06 01:45:42 +0100

    FIX

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
 
   delete '/logout', to: 'sessions#destroy'
-  post '/login', to: 'sessions#create'
+  post '/', to: 'sessions#create', as: 'login'
 	get '/profile', to: 'profiles#show', as: 'show_profile'
 	get '/answer', to: 'pages#answer'
   root 'pages#index'
